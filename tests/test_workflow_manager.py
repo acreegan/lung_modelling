ae5c214db365327a357fb170eba30f0b4304f75a
acreegan <andrew.s.creegan@gmail.com>
2023-10-12 16:37:29 +1300

    change slashes so tests work on posix

--- tests/test_workflow_manager.py
+++ tests/test_workflow_manager.py
@@ -8,34 +8,34 @@ def test_gather_directories():
     dirs1 = gather_directories(parent_dir / "test_data" / "test_dir_structure", data_folder_depth=3, skip_dirs=None,
                                select_dirs=None, index_list=None, show_progress=False)
 
-    correct_1 = [Path(r"a\c"), Path(r"a\d"), Path(r"b\c"), Path(r"b\d")]
+    correct_1 = [Path(r"a/c"), Path(r"a/d"), Path(r"b/c"), Path(r"b/d")]
     assert len(correct_1) == len(dirs1)
     for c1, d1 in zip(correct_1, [dir[0] for dir in dirs1]):
-        assert c1.as_posix() == d1.as_posix()
+        assert c1 == d1
 
     dirs2 = gather_directories(parent_dir / "test_data" / "test_dir_structure", data_folder_depth=3, skip_dirs=["*b*"],
                                select_dirs=None, index_list=None, show_progress=False)
 
-    correct_2 = [Path(r"a\c"), Path(r"a\d")]
+    correct_2 = [Path(r"a/c"), Path(r"a/d")]
     assert len(correct_2) == len(dirs2)
     for c1, d1 in zip(correct_2, [dir[0] for dir in dirs2]):
-        assert c1.as_posix() == d1.as_posix()
+        assert c1 == d1
 
     dirs3 = gather_directories(parent_dir / "test_data" / "test_dir_structure", data_folder_depth=3, skip_dirs=None,
                                select_dirs=["*b*"], index_list=None, show_progress=False)
 
-    correct_3 = [Path(r"b\c"), Path(r"b\d")]
+    correct_3 = [Path(r"b/c"), Path(r"b/d")]
     assert len(correct_3) == len(dirs3)
     for c1, d1 in zip(correct_3, [dir[0] for dir in dirs3]):
-        assert c1.as_posix() == d1.as_posix()
+        assert c1 == d1
 
     dirs4 = gather_directories(parent_dir / "test_data" / "test_dir_structure", data_folder_depth=3, skip_dirs=["*b*"],
                                select_dirs=["*c*"], index_list=None, show_progress=False)
 
-    correct_4 = [Path(r"a\c")]
+    correct_4 = [Path(r"a/c")]
     assert len(correct_4) == len(dirs4)
     for c1, d1 in zip(correct_4, [dir[0] for dir in dirs4]):
-        assert c1.as_posix() == d1.as_posix()
+        assert c1 == d1
 
 
 def test_dataloc():
