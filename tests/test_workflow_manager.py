7ed4d8dedf05611df30bd25cf2a465f788b2efbc
acreegan <andrew.s.creegan@gmail.com>
2023-11-10 12:01:41 +1300

    update test_gather_directories so order doesn't matter. Hopefully wil fix test in linux

--- tests/test_workflow_manager.py
+++ tests/test_workflow_manager.py
@@ -8,34 +8,39 @@ def test_gather_directories():
     dirs1 = gather_directories(parent_dir / "test_data" / "test_dir_structure", data_folder_depth=3, skip_dirs=None,
                                select_dirs=None, index_list=None, show_progress=False)
 
+    dirs1 = [dir[0] for dir in dirs1]
+
     correct_1 = [Path(r"a/c"), Path(r"a/d"), Path(r"b/c"), Path(r"b/d")]
     assert len(correct_1) == len(dirs1)
-    for c1, d1 in zip(correct_1, [dir[0] for dir in dirs1]):
-        assert c1 == d1
+    for c1 in correct_1:
+        assert c1 in dirs1
 
     dirs2 = gather_directories(parent_dir / "test_data" / "test_dir_structure", data_folder_depth=3, skip_dirs=["*b*"],
                                select_dirs=None, index_list=None, show_progress=False)
+    dirs2 = [dir[0] for dir in dirs2]
 
     correct_2 = [Path(r"a/c"), Path(r"a/d")]
     assert len(correct_2) == len(dirs2)
-    for c1, d1 in zip(correct_2, [dir[0] for dir in dirs2]):
-        assert c1 == d1
+    for c2 in correct_2:
+        assert c2 in dirs2
 
     dirs3 = gather_directories(parent_dir / "test_data" / "test_dir_structure", data_folder_depth=3, skip_dirs=None,
                                select_dirs=["*b*"], index_list=None, show_progress=False)
+    dirs3 = [dir[0] for dir in dirs3]
 
     correct_3 = [Path(r"b/c"), Path(r"b/d")]
     assert len(correct_3) == len(dirs3)
-    for c1, d1 in zip(correct_3, [dir[0] for dir in dirs3]):
-        assert c1 == d1
+    for c3 in correct_3:
+        assert c3 in dirs3
 
     dirs4 = gather_directories(parent_dir / "test_data" / "test_dir_structure", data_folder_depth=3, skip_dirs=["*b*"],
                                select_dirs=["*c*"], index_list=None, show_progress=False)
+    dirs4 = [dir[0] for dir in dirs4]
 
     correct_4 = [Path(r"a/c")]
     assert len(correct_4) == len(dirs4)
-    for c1, d1 in zip(correct_4, [dir[0] for dir in dirs4]):
-        assert c1 == d1
+    for c4 in correct_4:
+        assert c4 in dirs4
 
 
 def test_dataloc():
