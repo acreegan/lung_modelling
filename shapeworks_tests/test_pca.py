cd588a817a596ee3e47907346fa6cf72805dc4e0
acreegan <andrew.s.creegan@gmail.com>
2024-02-14 12:59:51 +1300

    PCA WIP before I get rid of stdev stuff for now

--- shapeworks_tests/test_pca.py
+++ shapeworks_tests/test_pca.py
@@ -11,6 +11,7 @@ import tempfile
 from pathlib import Path
 from glob import glob
 import pyacvd
+from sklearn.decomposition import PCA
 
 parent_dir = Path(__file__).parent
 
@@ -79,16 +80,6 @@ def test_pca_ellipsoid():
     pv_points = [pv.PointSet(p) for p in particles]
     points = np.array([p.points for p in pv_points])
 
-    # # Transform in x just for fun
-    # points[:, :, 0] += 2
-    #
-    # # Check correctness of transform
-    # p = pv.Plotter()
-    # p.add_points(pv_points[0], color="red")
-    # p.add_points(points[0], color="blue")
-    # p.show_bounds()
-    # p.show()
-
     embedder = PCA_Embbeder(points, num_dim=len(points) - 1)
 
     mean_data = embedder.mean_data
@@ -96,8 +87,8 @@ def test_pca_ellipsoid():
 
     np.testing.assert_allclose(project_zeros, mean_data)
 
-    for scores, points in zip(embedder.PCA_scores, points):
-        np.testing.assert_allclose(embedder.project(scores), points)
+    for scores, p in zip(embedder.PCA_scores, points):
+        np.testing.assert_allclose(embedder.project(scores), p)
 
     # # Plot meshes
     # c = matplotlib.colormaps["viridis"]
@@ -114,17 +105,17 @@ def test_pca_ellipsoid():
     # p.link_views()
     # p.show()
 
-    # # Plot meshes overlapping
-    # c = matplotlib.colormaps["viridis"]
-    # p = pv.Plotter()
-    # for i, points in enumerate(pv_points):
-    #     p.add_mesh(points, color=c((i * len(pv_points) / c.N) % c.N), label=f"ellipse {i + 1}",
-    #                render_points_as_spheres=True, point_size=5)
-    #
-    # p.show_axes()
-    # p.add_legend()
-    # p.show_bounds()
-    # p.show()
+    # Plot meshes overlapping
+    c = matplotlib.colormaps["viridis"]
+    p = pv.Plotter()
+    for i, points in enumerate(pv_points):
+        p.add_mesh(points, color=c((i * len(pv_points) / c.N) % c.N), label=f"ellipse {i + 1}",
+                   render_points_as_spheres=True, point_size=5)
+
+    p.show_axes()
+    p.add_legend()
+    p.show_bounds()
+    p.show()
 
     # # Plot eigenvectors
     # p = pv.Plotter(shape=(1, len(pv_points)), window_size=(5 * 300, 300))
@@ -183,33 +174,22 @@ def test_pca_project_with_stdev():
     # Prepare meshes with known stdev
     std = 0.5
     mean = 1.5
-    n_samples = 20
+    n_samples = 40
 
     rng = np.random.default_rng(0)
-    scales = np.sort(rng.normal(mean, std, n_samples))
+    scales = rng.normal(mean, std, n_samples)
+    scales = np.sort(scales)
 
     meshes = []
     for scale in scales:
         mesh = pv.Sphere(theta_resolution=20, phi_resolution=20, radius=1.5, center=[0, 0, 0]).scale([scale, 1, 1],
-                                                                                                       inplace=False)
+                                                                                                     inplace=False)
         meshes.append(mesh)
 
-    # # # Remesh and decimate to avoid strange correlations
-    # This is stupid. you could maybe do it before creating all the meshes. But we need to maintain correspondence
-    # remeshed = []
-    # for mesh in meshes:
-    #     clus = pyacvd.Clustering(mesh)
-    #     # clus.subdivide(3)
-    #     clus.cluster(20000)
-    #     remesh = clus.create_mesh()
-    #     remeshed.append(remesh)
-    #
-    # all_points = np.array([mesh.points for mesh in remeshed])
-    # # all_points = np.array([mesh.points for mesh in meshes])
-    # # decimated_points = all_points
-    # decimated_points = np.array([points[rng.integers(0, len(points), 400)] for points in all_points])
-
     points = np.array([mesh.points for mesh in meshes])
+    # Add some noise. The test fails without this
+    points = points + rng.normal(0, 0.01, points.shape)
+
     pv_points = [pv.PointSet(points) for points in points]
     embedder = PCA_Embbeder(points, num_dim=len(meshes) - 1)
 
@@ -218,28 +198,48 @@ def test_pca_project_with_stdev():
 
     np.testing.assert_allclose(project_zeros, mean_data)
 
-    for scores, points in zip(embedder.PCA_scores, points):
-        np.testing.assert_allclose(embedder.project(scores), points)
+    for scores, p in zip(embedder.PCA_scores, points):
+        np.testing.assert_allclose(embedder.project(scores), p)
 
+    # Test with stdevs
     largest_scale = scales[-1]
     stds_from_mean = (largest_scale - mean) / std
-    a = np.zeros(len(meshes) - 1)
-    a[0] = stds_from_mean
+    test_scores = np.zeros(len(meshes) - 1)
+    test_scores[0] = stds_from_mean
 
-    b = embedder.project_with_stdev(a)
-    score_stdevs = np.std(embedder.PCA_scores, axis=0)
+    projected = embedder.project_with_stdev(test_scores)
 
-    # Plot meshes overlapping
-    c = matplotlib.colormaps["viridis"]
-    p = pv.Plotter()
-    for i, points in enumerate(pv_points):
-        p.add_mesh(points, color=c((i * len(pv_points) / c.N) % c.N), label=f"ellipse {i + 1}",
-                   render_points_as_spheres=True, point_size=5)
+    scale_max, scale_min = np.max(scales), np.min(scales)
+    score_max, score_min = np.max(embedder.PCA_scores[:, 0]), np.min(embedder.PCA_scores[:, 0])
 
-    p.show_axes()
-    p.add_legend()
-    p.show_bounds()
-    p.show()
+    scale_std = np.std(scales)
+    score_std = np.std(embedder.PCA_scores[:, 0])
+
+    largest_scale_in_score_stds = stds_from_mean * score_std
+
+    pca = PCA(svd_solver="auto")
+    centered_points = points
+    pca_loadings = pca.fit_transform(centered_points.reshape([centered_points.shape[0], -1]))
+
+    np.testing.assert_allclose(pca_loadings[:, 0], embedder.PCA_scores[:, 0])
+
+    for scores, p in zip(pca_loadings, points):
+        np.testing.assert_allclose(pca.inverse_transform(scores).reshape([-1, 3]), p)
+
+    print("hello")
+    pass
+
+    # # Plot meshes overlapping
+    # c = matplotlib.colormaps["viridis"]
+    # p = pv.Plotter()
+    # for i, points in enumerate(pv_points):
+    #     p.add_mesh(points, color=c((i * len(pv_points) / c.N) % c.N), label=f"ellipse {i + 1}",
+    #                render_points_as_spheres=True, point_size=5)
+    #
+    # p.show_axes()
+    # p.add_legend()
+    # p.show_bounds()
+    # p.show()
 
     # # Plot eigenvectors
     # p = pv.Plotter(shape=(1, len(meshes)), window_size=(5 * 300, 300))
